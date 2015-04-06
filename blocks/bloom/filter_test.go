c311bda86bf6d3ba0644d5fa36b13d2f8acfcd24
Kristoffer Ström <kristoffer@rymdkoloni.se>
2015-04-06 16:45:23 +0200

    Make bloom filters simpler
    These did not work before, and had some unnecessary complexity.
    
    Now the filters use only one hashing function, no bignum arithmetic, and gets the additional bit positions by repeatedly hashing the result of prior hash.
    
    Since we're not concerned about crypto hashing here, this should be a win.
    
    External interfaces unchanged.

--- blocks/bloom/filter_test.go
+++ blocks/bloom/filter_test.go
@@ -1,14 +1,21 @@
 package bloom
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestFilter(t *testing.T) {
-	f := BasicFilter()
+	f := NewFilter(128)
+
 	keys := [][]byte{
 		[]byte("hello"),
 		[]byte("fish"),
 		[]byte("ipfsrocks"),
+		[]byte("i want ipfs socks"),
 	}
+
+	// fmt.Println(f)
 
 	f.Add(keys[0])
 	if !f.Find(keys[0]) {
@@ -21,10 +28,18 @@ func TestFilter(t *testing.T) {
 	}
 
 	f.Add(keys[2])
+	f.Add(keys[3])
 
 	for _, k := range keys {
 		if !f.Find(k) {
 			t.Fatal("Couldnt find one of three keys")
 		}
 	}
+
+	if f.Find([]byte("beep boop")) {
+		t.Fatal("Got false positive! Super unlikely!")
+	}
+
+	fmt.Println(f)
+
 }
